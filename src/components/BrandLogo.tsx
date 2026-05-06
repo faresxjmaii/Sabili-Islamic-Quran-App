a377dc530865887c92699cba2ac934efbadea0a6
Fares Jmai <faresxjmaii@users.noreply.github.com>
2026-05-06 19:53:24 +0200

    Fix Vercel logo deployment mismatch

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -9,11 +9,11 @@ export default function BrandLogo({ compact = false }: BrandLogoProps) {
   const { t } = useI18n();
 
   return (
-    <Link to="/" className="group flex shrink-0 items-center">
+    <Link to="/" className="group flex shrink-0 items-center py-1">
       <img
         src="/brand/sabili-logo.png"
         alt={t('appName')}
-        className={compact ? 'h-auto w-[108px] object-contain' : 'h-auto w-[150px] object-contain'}
+        className={compact ? 'h-[23px] w-auto object-contain opacity-85 transition-opacity group-hover:opacity-95' : 'h-[28px] w-auto object-contain opacity-85 transition-opacity group-hover:opacity-95'}
         draggable={false}
       />
     </Link>
